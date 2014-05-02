147c724ccb9d7aa221148ed039ad545da8745542
Jörn Friedrich Dreyer <jfd@butonic.de>
2014-05-02 10:49:07 +0200

    fix unit tests

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -3,7 +3,9 @@
 global $RUNTIME_NOAPPS;
 $RUNTIME_NOAPPS = true;
 
-define('PHPUNIT_RUN', 1);
+if (!defined('PHPUNIT_RUN')) {
+	define('PHPUNIT_RUN', 1);
+}
 
 require_once __DIR__.'/../../../../lib/base.php';
 
@@ -14,9 +16,6 @@ if(!class_exists('PHPUnit_Framework_TestCase')) {
 //add 3rdparty folder to include path
 $dir = __DIR__.'/../../3rdparty';
 set_include_path(get_include_path() . PATH_SEPARATOR . $dir);
-
-OC::$CLASSPATH['Zend_Search_Lucene_Interface'] = 'search_lucene/3rdparty/Zend/Search/Lucene/Interface.php';
-OC::$CLASSPATH['Zend_Search_Lucene_Search_QueryHit'] = 'search_lucene/3rdparty/Zend/Search/Lucene/Search/QueryHit.php';
 
 OC_Hook::clear();
 OC_Log::$enabled = false;
