bfd54aaf875a9f66073c5e05bc15213d5ccee5bb
Pierre Jochem <pierrejochem@msn.com>
2015-09-09 14:49:33 +0200

    New PR for Problem solution for cron job error ;)
    Solves a problem with a cron job: #106

--- db/statusmapper.php
+++ db/statusmapper.php
@@ -169,17 +169,16 @@ class StatusMapper extends Mapper {
 				$this->logger->
 					debug( 'expected string or instance of \OC\Files\Mount\Mount got ' . json_encode($mount) );
 			}
-			if($storage != null)
-				//only index local files for now
-				if ($storage->isLocal()) {
-					$cache = $storage->getCache();
-					$numericId = $cache->getNumericStorageId();
+			
+			//only index local files for now
+			if ($storage && $storage->isLocal()) {
+				$cache = $storage->getCache();
+				$numericId = $cache->getNumericStorageId();
 
-					$result = $query->execute(array($numericId, Status::STATUS_NEW));
-
-					while ($row = $result->fetchRow()) {
-						$files[] = $row['fileid'];
-					}
+				$result = $query->execute(array($numericId, Status::STATUS_NEW));
+					
+				while ($row = $result->fetchRow()) {
+					$files[] = $row['fileid'];
 				}
 			}
 		}
